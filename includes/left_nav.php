d0dcd64aff087183acf00a484211866f235f952b
dimiourgia <ajayaxes318@gmail.com>
2022-07-05 04:44:48 +0530

    Add files via upload

---
+++ includes/left_nav.php
@@ -0,0 +1,17 @@
+<div class="col-sm-3" style="">
+			<ul class="list-group">
+				 <li class="list-group-item">Dashboard</li>
+				 <li class="list-group-item"><a href="user_management.php">User Management</a></li>
+				<li class="list-group-item"><a href="user_management.php">Nav1</a></li>
+				
+				<li class="list-group-item"><a href="user_managemet.php">Nav1</a></li>
+				<li class="list-group-item"><a href="user_managemet.php">Nav1</a></li>
+				<li class="list-group-item"><a href="user_managemet.php">User Management</a></li>
+				<li class="list-group-item"><a href="user_managemet.php">User Management</a></li>
+				<li class="list-group-item"><a href="user_managemet.php">User Management</a></li>
+				<li class="list-group-item"><a href="user_managemet.php">User Management</a></li>
+					
+			</ul>
+
+	</div>
+   
